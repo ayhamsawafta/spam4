c1c405662e65292fe09e3695bf1084e2363cc2db
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-13 09:26:08 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', message => {
+if (message.content === 'e1') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 1000000000000000000000000000; x++) {
+        message.channel.send(`i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.login("NDYzODAwMDE2NTcwNjc5MzE2.DsXp6g.Yaw-Gmx6QbDRfvZiyXXFlXAGLfY");
